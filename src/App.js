7838a746dd3103dfc2817c71c7bb719e9dd1ac43
Yongki40 <yongkitanu@gmail.com>
2020-09-12 12:51:57 +0700

    Mapping components🚀🚀🚀

--- src/App.js
+++ src/App.js
@@ -1,36 +1,58 @@
 import React from 'react';
 import Jokes from './components/Jokes';
+import jokesData from './components/jokesData';
 import './App.css';
 
 function App() {
-  return (
-    <React.Fragment>
-          <Jokes
-            answer="Cuman punchline"
-        ></Jokes>
+    //mapping
+    // const nums =[1,2,3,4,5,6,7,8,9,10];
+    // const double = nums.map(function(num){
+    //     return num * 2
+    // });
+    // console.log(double);
 
-        <Jokes
-            ques="pertanyaan 1 ?"
-            answer="jawaban 1. Hahahahaha"
-        ></Jokes>
-        <Jokes
-            ques="pertanyaan 2 ?"
-            answer="jawaban 2. Hahahahaha"
-        ></Jokes>
-        <Jokes
-            ques="pertanyaan 3 ?"
-            answer="jawaban 3. Hahahahaha"
-        ></Jokes>
-        <Jokes
-            ques="pertanyaan 4 ?"
-            answer="jawaban 4. Hahahahaha"
-        ></Jokes>
-        <Jokes
-            ques="pertanyaan 5 ?"
-            answer="jawaban 5. Hahahahaha"
-        ></Jokes>
-    </React.Fragment>
-  );
+    //array of components
+    const jokesComponents = jokesData.map((joke)=>{
+        return (
+            <Jokes key={joke.id} ques={joke.ques} answer={joke.answer}/>
+        );
+    })
+    
+    return (
+        //cara biasa
+        // <React.Fragment>
+        //     <Jokes
+        //         answer="Cuman punchline"
+        //     ></Jokes>
+
+        //     <Jokes
+        //         ques="pertanyaan 1 ?"
+        //         answer="jawaban 1. Hahahahaha"
+        //     ></Jokes>
+        //     <Jokes
+        //         ques="pertanyaan 2 ?"
+        //         answer="jawaban 2. Hahahahaha"
+        //     ></Jokes>
+        //     <Jokes
+        //         ques="pertanyaan 3 ?"
+        //         answer="jawaban 3. Hahahahaha"
+        //     ></Jokes>
+        //     <Jokes
+        //         ques="pertanyaan 4 ?"
+        //         answer="jawaban 4. Hahahahaha"
+        //     ></Jokes>
+        //     <Jokes
+        //         ques="pertanyaan 5 ?"
+        //         answer="jawaban 5. Hahahahaha"
+        //     ></Jokes>
+        // </React.Fragment>
+
+        //cara dengan array of components
+
+        <React.Fragment>
+            {jokesComponents}
+        </React.Fragment>
+    );
 }
 
 export default App;
---
+++ src/components/jokesData.js
@@ -0,0 +1,29 @@
+const jokesData = [
+    {
+        id:1,
+        answer: "cuman jawaban saya"
+    },
+    {
+        id:2,
+        ques: "pertanyaan 1",
+        answer: "jawaban 1. Hahahahaha"
+    },
+    {
+        id:3,
+        ques: "pertanyaan 2",
+        answer: "jawaban 2. Hahahahaha"
+    },
+    {
+        id:4,
+        ques: "pertanyaan 3",
+        answer: "jawaban 3. Hahahahaha"
+    },
+    {
+        id:5,
+        ques: "pertanyaan 4",
+        answer: "jawaban 4. Hahahahaha"
+    }
+   
+]
+
+export default jokesData;
